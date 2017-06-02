e8b83f9f36f20dee9019ab4cbbab1f3a3c5caf53
st234pa <syoon1@stuy.edu>
2017-06-02 14:51:06 -0400

    changed some mutators/accessors, started propogation method

--- deploy/utils/graph.py
+++ deploy/utils/graph.py
@@ -9,22 +9,46 @@ class Course(object):
     # Course[] parents   :
     # Course[] children  : 
     # ============================================
-    def __init__(self, name, state,  parents=[], children=[]):
-        self.name = name
+    def __init__(self, code, state,  parents=[], numRequired, children=[]):
+        self.code = code
         self.state = state
-        self.parents = parents
+        self.prereqs = (parents, numRequired)
         self.children = children
         self.depth = -1 # Uninitialized
         
     # Mutators and Accessors
-    def getParents():
-        return self.parents
+    def getCode():
+        return self.code
+    def getState():
+        return self.state
+    def getPrereqs():
+        return self.prereqs
     def getChildren():
         return self.children
-    def getDepth(): # Is this necessary anymore?
+    def getDepth(): # This will probably have to change
         if (self.depth == -1):
-            if (len(getParents()) == 0):
+            if (len(getPrereqs()[0]) == 0):
                 self.depth = 0
-            self.depth = 1 + max([course.getDepth() for course in getParents()])
-        return self.depth 
+            self.depth = 1 + max([course.getDepth() for course in getPrereqs()[0]])
+        return self.depth
+    def setState(newState):
+        old = getState()
+        self.state = newState
+        return old
+
+    # Propogating Up
+    def propogate():
+        if getDepth() == 0:
+            return
+        if getState() == 1: # required/selected
+
+            if len(getPrereqs()[0]) == 1:
+                getPrereqs()[0][0].setState(1)
+                getPrereqs()[0][0].propogate()
+            else:
+               def propogateMaybe():
+                   # should work the same way for if current node's state is maybe
+                        
+            
+    
 
