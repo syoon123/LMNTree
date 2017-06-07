eba4edf3e84b2704ecfde696e7f6e9f63909f806
maxbert <mbertfield@stuy.edu>
2017-06-07 15:33:25 -0400

    serious progress

--- deploy/static/js/tree.js
+++ deploy/static/js/tree.js
@@ -6,7 +6,7 @@ var table = d3.csvParse(data, function(d,i){
 	parentId: d.Prereq  };
 });
 
-console.log(table);
+//console.log(table);
 
 var treeData = d3.stratify()(table);
 
@@ -17,23 +17,24 @@ var margin = {top: 20, right: 120, bottom: 20, left: 120},
 var i = 0;
 var currdepth = 1;
 var tree = d3.layout.tree()
- .size([height, width]);
+    .size([height, width]);
 
 var diagonal = d3.svg.diagonal()
- .projection(function(d) { return [d.y, d.x]; });
+    .projection(function(d) { return [d.y, d.x]; });
 
 var svg = d3.select("body").append("svg")
- .attr("width", width + margin.right + margin.left)
- .attr("height", height + margin.top + margin.bottom)
-  .append("g")
- .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 root = treeData;
-  
+
 update(root,1);
 function update(source,depth) {
-    //svg.selectAll("g.button").enter().append("g").attr('class','butt').append('rect').
-  // Compute the new tree layout.
+    for(i = 1; i<= depth; i++){
+	svg.append("circle").attr('class','buttons').attr('r',10).attr('x', 180).attr('y', 10).attr('selected',false).style("fill","green").attr('transform', 'translate(' + (i * 180) + ",0)");
+    };
   var nodes = tree.nodes(root).reverse(),
    links = tree.links(nodes);
 
@@ -50,14 +51,15 @@ function update(source,depth) {
 	    // Enter the nodes.
   var nodeEnter = node.enter().append("g")
       .attr("class", "node")
+    .attr("display", function(d){return d.depth > depth?'none':''})
       .on('click', function(d){d.selected = d.selected?false:true; updatecolor() } ) 
       .attr("transform", function(d) { 
 	  return "translate(" + d.y + "," + d.x + ")"; });
 
   nodeEnter.append("rect")
-	.attr("width", 35)
+	.attr("width", function(d){ return d.id.split('').length * 6;})
 	.attr("height", 20).attr("y",-10)
-	.attr("display",function(d){console.log(d.depth); d.depth <= depth?'none':'unset'})
+	.attr("display",function(d){d.depth <= depth?'none':'unset'})
 	.style("fill",function(d){return d.selected?"green":"#fff";})
 	.style("stroke-width","1")
 	.style("stroke","rgb(0,0,0)");
@@ -73,7 +75,7 @@ function update(source,depth) {
    .attr("text-anchor", function(d) { 
     return "start"; })
    .text(function(d) { return d.id; })
-   .style("fill-opacity", 1);
+   .style("fill-opacity", 1).style('font-size','10px');
 
   // Declare the linksâ€¦
   var link = svg.selectAll("path.link")
@@ -82,6 +84,7 @@ function update(source,depth) {
   // Enter the links.
   link.enter().insert("path", "g")
    .attr("class", "link")
+    .attr("display",function(d){return d.target.depth > depth?"none":""})
    .attr("d", diagonal);
 
 }
