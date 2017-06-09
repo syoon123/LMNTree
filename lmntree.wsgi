49956915a0f8bbe62a8ced1fc48ca5c5e6ba9a71
maxbert <mbertfield@stuy.edu>
2017-06-09 14:29:24 -0400

    deploy?

---
+++ lmntree.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import sys
+sys.path.insert(0,"/var/www/lmntree/")
+from lmntree import app as application
---
+++ lmntree/utils/course_selector.py
@@ -0,0 +1,10 @@
+import csv
+DIR = os.path.dirname('__file__') + "/"
+def convert_csv_to_dict():
+    f = open(DIR + "data/StuyCourses.csv")
+    dict = {}
+    for line in f.readlines():
+        list_of_classes = line.split(",")[1:]
+        dict[line.split(",")[0]] = list_of_classes
+    return dict
+    
